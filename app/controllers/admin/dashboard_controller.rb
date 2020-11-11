baf43d9c09ebfbfa27c8b16a64d49f532415c13b
Eric McGrandle <ericmcgrandle@gmail.com>
2020-11-11 18:01:23 +0000

    admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,12 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
 
   def show
+    @msg = "here"
+
+    @categories = Category.count
+    @categories_name = Category.all()
+    @products = Product.all()
+
+
   end
 end
