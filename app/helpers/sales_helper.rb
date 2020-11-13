35f65d7996f45ff04b31ead53fe5390d17cfb393
Eric McGrandle <ericmcgrandle@gmail.com>
2020-11-13 19:00:15 +0000

    sale price

--- app/helpers/sales_helper.rb
+++ app/helpers/sales_helper.rb
@@ -2,4 +2,8 @@ module SalesHelper
   def active_sale?
     Sale.active.all
   end
+  def new_price(old_price)
+    percent_off = active_sale?[0].percent_off
+    new_price = old_price * ((100.00 - percent_off) / 100)
+  end
 end
