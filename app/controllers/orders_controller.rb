d1c89164aae74e5b57065decfefa1cf981e7b9c7
Eric McGrandle <ericmcgrandle@gmail.com>
2020-11-13 23:29:15 +0000

    99% func. Bug with way total is collected

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -27,35 +27,49 @@ class OrdersController < ApplicationController
 
   private
 
+  def price(old_price)
+    if Sale.active.any?
+      percent_off = Sale.active.all[0].percent_off
+      new_price = old_price * ((100.00 - percent_off) / 100)
+    else
+      old_price
+    end
+  end
+
   def empty_cart!
     # empty hash means no products in cart :)
     update_cart({})
   end
 
   def perform_stripe_charge
+    price = price(cart_subtotal_cents).floor
+    puts cart_subtotal_cents
+    puts price
     Stripe::Charge.create(
       source:      params[:stripeToken],
-      amount:      cart_subtotal_cents,
+      amount:      price,
       description: "Khurram Virani's Jungle Order",
       currency:    'cad'
     )
   end
 
   def create_order(stripe_charge)
+    price = price(cart_subtotal_cents)
     order = Order.new(
       email: params[:stripeEmail],
-      total_cents: cart_subtotal_cents,
+      total_cents: price,
       stripe_charge_id: stripe_charge.id, # returned by stripe
     )
 
     enhanced_cart.each do |entry|
       product = entry[:product]
       quantity = entry[:quantity]
+      price = price(product.price)
       order.line_items.new(
         product: product,
         quantity: quantity,
-        item_price: product.price,
-        total_price: product.price * quantity
+        item_price: price,
+        total_price: price * quantity
       )
     end
     order.save!
