7e7f234a8f08455186d087ffa4dc4fb6dea80e33
Eric McGrandle <ericmcgrandle@gmail.com>
2020-11-14 04:23:57 +0000

    saves successful

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,9 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
+    # Create Category and Product
+    let(:create_category) { Category.create(name: "RSpec Test") }
+    let(:create_product) { Product.new({ name: "Rspec", description: "Test", price_cents: 10000, quantity: 10, category_id: create_category.id }) }
+    
     it "saves successfully" do
-      
-    end
+      expect do
+        create_category.save
+      end.to change{ Category.count }.by(1)
+      expect do
+        create_product.save
+      end.to change{ Product.count }.by(1)
 
+    end
   end
 end
