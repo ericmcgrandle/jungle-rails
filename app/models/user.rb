7255cc17f49e60f9a050760e01a3f5a348d171b8
Eric McGrandle <ericmcgrandle@gmail.com>
2020-11-14 23:03:09 +0000

    user_spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,26 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  before_validation :strip_whitespace
+  before_save :downcase
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+
+  private
+  def strip_whitespace
+    self.email = self.email.strip unless self.email.nil?
+  end
+
+  def downcase
+    self.email = self.email.downcase unless self.email.nil?
+  end
+
+  def self.find_by_lower_email(email)
+    User.find_by_email(email.downcase)
+  end
+
 end
