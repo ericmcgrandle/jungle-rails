81c3ca3217444ca69ac0c75b656e42e32950b7ef
Eric McGrandle <ericmcgrandle@gmail.com>
2020-11-18 00:45:03 +0000

    styling

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,7 @@
 class ReviewsController < ApplicationController
   before_filter :authorize
-  def create
 
+  def create
     product_id = params[:product_id]
     description = params[:products][:description]
     rating = params[:products][:rating]
@@ -12,16 +12,16 @@ class ReviewsController < ApplicationController
       @review.user_id = current_user[:id]
     end
     
-
-    
     if @review.save
       redirect_to "/products/#{product_id}", notice: 'Product created!'
     else
       redirect_to "/products/#{product_id}", notice: 'Product NOT created!'    
     end
+  end
 
-
-
-    
+  def destroy
+    @review = Review.find params[:id]
+    @review.destroy
+    redirect_to "/products/#{params[:product_id]}", notice: "Deleted"
   end
 end
